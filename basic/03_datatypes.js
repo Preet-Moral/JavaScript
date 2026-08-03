f95991a68991e0bc36039a03b7e68b02389259db
Preet Moral <preetm.ic.24@nitj.ac.in>
2026-08-03 10:43:42 +0000

    done

---
+++ basic/03_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JS code as newer version 
+
+// alert(2+3) , we are using nodejs , not browser it works on browser
+console.log("Preet")
+console.log(3+      // readibility
+    3
+)
+
+// Datatypes
+let name="preet"
+let age=21
+let isLoggedIn=false
+let a=null
+let b=undefined
+
+// 1.) Number -> till 2 to power 53
+// 2.)bigint
+// 3.)string -> ""
+// 4.)bool -> true/false
+// 5.)null -> standalone value
+// 6.)undefined
+// 7.)symbol -> unique
+// 8.)object
+
+// Interesting thing
+console.log(typeof age)
+console.log(typeof null) // -> object intead of null 
+console.log(typeof undefined) // -> undefined
