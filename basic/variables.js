1281688f6c0a79c700c0b7c049b6b8c61eec512b
Preet Moral <preetm.ic.24@nitj.ac.in>
2026-08-03 09:54:07 +0000

    done

---
+++ basic/variables.js
@@ -0,0 +1,12 @@
+const accountId=144213
+let accountEmail="preetmoral567@gmail.com"
+var accountPassword="12345"
+accountCity="Meerut"
+accountEmail="abc@gmail.com"
+accountPassword="12"
+accountCity="Greater Noida"
+let accountState;
+// accountId=68 changes in const is not allowed
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+// prefer to use let , not to use var bcoz of block scope
+// and functional scope and never declare without const / var / let as in last 
