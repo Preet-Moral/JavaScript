f2fcc9b2b9751e00c478aa60183d985310d895c9
Preet Moral <preetm.ic.24@nitj.ac.in>
2026-08-03 11:33:12 +0000

    ok

---
+++ basic/04_conversion.js
@@ -0,0 +1,25 @@
+let score=undefined
+
+console.log(typeof score)
+let valInNumber=Number(score)
+console.log(valInNumber);
+console.log(typeof valInNumber);
+
+// "33" -> 33
+// "33abc" -> NaN not a number
+// true -> 1 and false -> 0
+
+let login=4
+let boolLogin=Boolean (login)
+console.log(typeof boolLogin);
+console.log(boolLogin);
+
+// in bool 
+// "" -> false
+// "Preet" -> true
+// 1,2,.. -> true and 0 -> false
+
+let num=34
+let strNum=String(num);
+console.log(strNum);
+console.log(typeof strNum);
