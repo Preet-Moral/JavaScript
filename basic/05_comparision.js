931532277acf34d3a5f7cb49392d99aa2d78e84f
Preet Moral <preetm.ic.24@nitj.ac.in>
2026-08-08 10:06:24 +0000

    ok

---
+++ basic/05_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 <= 1);
+// console.log(2 != 1);
+
+// console.log("2">1);
+// console.log("02">1);
+// console.log("1">2);
+
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+console.log(null < 0)
+// > , < , >= , <= works different than  == , they convert null to zero 
+console.log(undefined >0);
+console.log(undefined ==0);
+console.log(undefined <0);
+
+// === operator  compares datatypes as well 
+console.log("2" > 2)
