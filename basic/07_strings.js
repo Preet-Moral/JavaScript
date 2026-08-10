1d62c45c603dc4317db33c9bb7c762ef42cc913a
Preet Moral <preetm.ic.24@nitj.ac.in>
2026-08-10 11:04:25 +0000

    str

---
+++ basic/07_strings.js
@@ -0,0 +1,33 @@
+const name="Preet Moral"
+const repo=50
+//console.log(name + repo + " Value");
+console.log(`my name is ${name} and my repo count is ${repo}`);
+// use `---- ` these are used in modern days
+
+const newgame=new String("Preet")
+console.log(newgame[0]);
+console.log(newgame.__proto__)
+
+console.log(newgame.length);
+console.log(newgame.toUpperCase());
+
+console.log(newgame.charAt(2));
+console.log(newgame.indexOf('e'));
+
+const newest=newgame.substring(0,2);
+console.log(newest);
+
+const naya=newgame.slice(-100,1);
+console.log(naya);
+
+const obj="   P  r   e   e   t"
+console.log(obj);
+console.log(obj.trim());
+
+const naam = "misssipi"
+console.log(naam.replaceAll('i','x'));
+console.log(naam.includes("pi"))
+// tell whether a element is present or not 
+
+console.log(name.split(' '))
+// splits when find te required character 
