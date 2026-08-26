f0f06d13b66c9e41d52eaa017a5c680d6aefe1f2
Preet Moral <preetm.ic.24@nitj.ac.in>
2026-08-26 18:11:32 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,13 @@
+const a = 1; 
+ if(true){const a = 10;
+            var b = 20;
+            let c = 30;
+            console.log(a);
+        }
+console.log(a)
+// console.log(b)
+// console.log(c)
+
+// var causes problem of scope 
+
+
